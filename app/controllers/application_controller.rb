39e74868e1aa69fa33e4a2c520cd6e4ef87ddb65
esun33 <eleanor.sun@chicagobooth.edu>
2022-10-23 20:49:09 +0000

    Passed rails grade Final commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,45 @@
+require "open-uri"
+require "json"
+
 class ApplicationController < ActionController::Base
+  def homepage
+    render({ :template => "homepage_template.html.erb"})
+  end
+
+  def currency_pairs
+    symbols_endpoint = "https://api.exchangerate.host/symbols"
+    raw_data = URI.open(symbols_endpoint).read
+    parsed_data = JSON.parse(raw_data)
+    symbols_hash = parsed_data.fetch("symbols")
+
+    @symbols_list = symbols_hash.keys
+
+    render({ :template => "currency_pairs_template.html.erb"})
+  end
+
+  def conversion_pairs
+    @current_symbol = params.fetch("this_symbol")
+
+    symbols_endpoint = "https://api.exchangerate.host/symbols"
+    raw_data = URI.open(symbols_endpoint).read
+    parsed_data = JSON.parse(raw_data)
+    symbols_hash = parsed_data.fetch("symbols")
+
+    @symbols_list = symbols_hash.keys
+
+    render({ :template => "conversion_pairs_template.html.erb"})
+  end
+
+  def conversion_results
+    @symbol1 = params.fetch("symbol_from")
+    @symbol2 = params.fetch("symbol_to")
+
+    conversion_endpoint = "https://api.exchangerate.host/convert?from=#{@symbol1}&to=#{@symbol2}"
+    
+    raw_data = URI.open(conversion_endpoint).read
+    @parsed_data = JSON.parse(raw_data)
+    @exchange_rate = @parsed_data.fetch("result")
+
+    render({ :template => "conversion_results_template.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #Homepage route
+  get("/", { :controller => "application", :action => "homepage"})
+
+  #Foreign exchange
+  get("/forex", { :controller => "application", :action => "currency_pairs"})
+  get("/forex/:this_symbol", { :controller => "application", :action => "conversion_pairs"})
+  get("/forex/:symbol_from/:symbol_to", { :controller => "application", :action => "conversion_results"})
 end
